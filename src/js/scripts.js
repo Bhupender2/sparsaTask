d9a4281f49f74d8315cba6189b0858cde53e288b
Bhupender2 <b.mehtaa2021@gmail.com>
2024-06-14 03:28:08 +0530

    added animation percetly

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,15 +1,18 @@
 // this is the animation scripts
 
 document.addEventListener("DOMContentLoaded", function () {
-  const words = ["developers 🥲", "graphic-designers 🍿", "coders 🧑‍💻"];
+  const words = ["developers 🥲", "graphic-designers 🍿", "coders 🧑‍💻", "Vampires 🧛"];
   let index = 0;
   const animatedText = document.getElementById("animated-text");
 
   function changeWord() {
-    animatedText.textContent = words[index];
-    index = (index + 1) % words.length;
+    animatedText.classList.add('hidden');
+    setTimeout(() => {
+      index = (index + 1) % words.length;
+      animatedText.textContent = words[index];
+      animatedText.classList.remove('hidden');
+    }, 1000); // Time it takes for the fade out transition
   }
-
-  setInterval(changeWord, 3000); // Change every 3 seconds
+  setInterval(changeWord, 2000); // Change every 3 seconds
   changeWord(); // Initial call
 });
